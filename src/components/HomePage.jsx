2dcfcc03d749f889f92437261016d80fbf47fe0d
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2025-04-10 19:41:01 -0500

    Agregar categorias a homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import ImagenPrincipal from "./ImagenPrincipal";
+import Category from "./Category";
+import useData from "../hooks/useData";
 
 export default function HomePage() {
-  return <ImagenPrincipal />;
+  const { response } = useData(
+    "https://www.themealdb.com/api/json/v1/1/categories.php"
+  );
+
+  console.log(response?.categories);
+  const comidas = response?.categories || [];
+
+  return (
+    <>
+      <ImagenPrincipal />
+      <Category comidas={comidas} />
+    </>
+  );
 }
