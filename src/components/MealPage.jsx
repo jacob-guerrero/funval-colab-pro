99e5d86b47380bf3127ada2cab45f1073dc26c37
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2025-04-11 15:58:35 -0500

    Arreglar componentes en merge

--- src/components/MealPage.jsx
+++ src/components/MealPage.jsx
@@ -4,9 +4,10 @@ import ImagePlato from "./ImagePlato";
 import VideoPlato from "./VideoPlato";
 import { useParams } from "react-router";
 import useData from "../hooks/useData";
-export default function MealPage({ }) {
+
+export default function MealPage() {
   const params = useParams();
-  const { loading, error, response } = useData(
+  const { response } = useData(
     `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.id}`
   );
   const imagen = response?.meals[0].strMealThumb || "";
@@ -14,23 +15,20 @@ export default function MealPage({ }) {
   const titulo = response?.meals[0].strMeal || "";
   return (
     <>
-      <h1 className="font-bold text-[22px] md:text-3xl py-1 text-center bg-amber-100 flex justify-center items-center text-amber-700">{titulo}<img src="/plato.png" alt="" className="w-10 h-10 mx-3" /></h1>
+      <h1 className="font-bold text-[22px] md:text-3xl py-1 text-center bg-amber-100 flex justify-center items-center text-amber-700">
+        {titulo}
+        <img src="/plato.png" alt="" className="w-10 h-10 mx-3" />
+      </h1>
       <div className="md:flex ">
         <div className="m-2 md:w-1/2 shadow-md shadow-orange-300 rounded-2xl bg-amber-50 ">
-          <IngreInstru
-            response={response}
-          />
+          <IngreInstru response={response} />
         </div>
         <div className=" m-2 md:w-1/2 shadow-md shadow-orange-300 rounded-2xl bg-amber-50 ">
           <div className="flex items-center h-70 m-2 ">
-            <ImagePlato
-              src={imagen}
-            />
+            <ImagePlato src={imagen} />
           </div>
           <div className="flex items-center h-70 m-2 ">
-            <VideoPlato
-              youtubeUrl={video}
-            />
+            <VideoPlato youtubeUrl={video} />
           </div>
         </div>
       </div>
